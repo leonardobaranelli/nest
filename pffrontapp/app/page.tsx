2bebfd0861cd761a3a8c7d3da80319a6d2a4a879
Gustacm <gustavocastilloma@gmail.com>
2023-10-25 10:46:08 -0300

    redux configuration

--- pffrontapp/app/page.tsx
+++ pffrontapp/app/page.tsx
@@ -1,8 +1,29 @@
+"use client";
+
 import React from 'react'
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import { decrement, increment, reset } from "@/redux/features/counterSlice";
 
 function page() {
+const count = useAppSelector((state) => state.counterReducer.value);
+  const dispatch = useAppDispatch();
+
   return (
-    <div>page</div>
+    <div>page
+            <div>
+        <h4 style={{ marginBottom: 16 }}>{count}</h4>
+        <button onClick={() => dispatch(increment())}>increment</button>
+        <button
+          onClick={() => dispatch(decrement())}
+          style={{ marginInline: 16 }}
+        >
+          decrement
+        </button>
+        <button onClick={() => dispatch(reset())}>reset</button>
+      </div>
+
+
+    </div>
   )
 }
 
