5dcfff41f5112b5fffe9ad3ca86e825835eb67ab
Gustacm <gustavocastilloma@gmail.com>
2023-10-25 11:00:07 -0300

    redux configuration

--- pffrontapp/app/page.tsx
+++ pffrontapp/app/page.tsx
@@ -9,7 +9,7 @@ const count = useAppSelector((state) => state.counterReducer.value);
   const dispatch = useAppDispatch();
 
   return (
-    <div>page
+    
             <div>
         <h4 style={{ marginBottom: 16 }}>{count}</h4>
         <button onClick={() => dispatch(increment())}>increment</button>
@@ -23,7 +23,7 @@ const count = useAppSelector((state) => state.counterReducer.value);
       </div>
 
 
-    </div>
+    
   )
 }
 
