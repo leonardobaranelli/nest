2fc8a3622243901830ecfd251e5a0cca176d4f9d
JuanMPaola <juanmpaola25@gmail.com>
2023-10-26 11:00:14 -0300

    chucha

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { sequelize } from './shared/models/relations.config';
@@ -6,6 +7,11 @@ async function bootstrap() {
   await sequelize.sync();
 
   const app = await NestFactory.create(AppModule);
+
+app.useGlobalPipes( new ValidationPipe({
+  transform: true,
+}))
+
   await app.listen(3000);
 }
 bootstrap();
