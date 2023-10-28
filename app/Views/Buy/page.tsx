285019bff2569ca0b4d48263514c7900b548708e
Gustacm <gustavocastilloma@gmail.com>
2023-10-28 13:51:02 +0200

    modification of the information and redux implementation

--- app/Views/Buy/page.tsx
+++ app/Views/Buy/page.tsx
@@ -1,40 +1,38 @@
-import React from 'react';
+"use client";
+
+import React, { useEffect } from 'react';
 import Cards from '@/app/components/Cards/Cards';
 import Link from 'next/link';
-import axios from "axios";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import { useGetPostsByConditionQuery } from "@/redux/features/Post";
 
 const Page = () => {
-  const fetchData = async () => {
-    try {
-      const response = await axios.get('http://localhost:3001/posts/sell');
-      const data = response.data;
-      
-      return data;
-    } catch (error) {
-      console.error('Error al obtener datos:', error);
-      return [];
+  const dispatch = useAppDispatch();
+  const { data, isLoading, isError } = useGetPostsByConditionQuery("sell");
+
+  useEffect(() => {
+    if (!isLoading && !isError) {
+      console.log("Data from API:", data); // Agregar el console.log aquí
     }
-  };
+  }, [isLoading, isError, dispatch]);
 
-  const renderWithData = async () => {
-    const properties = await fetchData();    
-    return (
-      <div>
-        <button>
-          <Link href="/Views/home">Home</Link>
-        </button>
-        <Cards properties={properties} />
-      </div>
-    );
-  };
-
-  return renderWithData();
+  return (
+    <div>
+      <button>
+        <Link href="/Views/home">Home</Link>
+      </button>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : isError ? (
+        <p>Error al obtener datos</p>
+      ) : (
+        <Cards properties={data as Post[]} />
+      )}
+    </div>
+  );
 };
 
 export default Page;
-
-
-
 
 
 
