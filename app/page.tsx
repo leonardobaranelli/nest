208cfd3e9f09434b1246d26509f85da3eeb1f3c5
fran <fran22345@gmail.com>
2023-10-28 15:44:11 -0300

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,13 @@
 "use client";
 import React from "react";
 import Landing from "./components/Landing/Landing";
-import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+
 
 
 function page() {
-const count = useAppSelector((state) => state.counterReducer.value);
-  const dispatch = useAppDispatch();
 
-  return (
-    <div>
-      <Landing />
+  return 
 
-      <h4 style={{ marginBottom: 16 }}>{count}</h4>
-      <button onClick={() => dispatch(increment())}>increment</button>
-      <button
-        onClick={() => dispatch(decrement())}
-        style={{ marginInline: 16 }}
-      >
-        decrement
-      </button>
-      <button onClick={() => dispatch(reset())}>reset</button>
-    </div>
-  );
 }
 
 export default page;
