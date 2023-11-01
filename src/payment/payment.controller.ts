dde0ae22c0489bf12f66e22f86e0e1f7e27b2d9e
leonardobaranelli <leobaranelli@gmail.com>
2023-11-01 17:16:48 -0300

    chore: update (and sync with dev db)

--- src/payment/payment.controller.ts
+++ src/payment/payment.controller.ts
@@ -5,41 +5,41 @@ import { PaymentService, ProductData } from './payment.service';
 export class PaymentController {
   constructor(private readonly paymentService: PaymentService) {}
 
-  @Get('charges')
-  async getCharges(): Promise<any> {    
-    const { stripeCharges } = await this.paymentService.getCharges();
-    return {
-      stripeCharges,
-    };
-  }
+  // @Get('charges')
+  // async getCharges(): Promise<any> {    
+  //   const { stripeCharges } = await this.paymentService.getCharges();
+  //   return {
+  //     stripeCharges,
+  //   };
+  // }
 
-  @Get('prices')
-  async getPrices(): Promise<any> {    
-    const { stripePrices } = await this.paymentService.getPrices();
-    return {
-      stripePrices,
-    };
-  }
+  // @Get('prices')
+  // async getPrices(): Promise<any> {    
+  //   const { stripePrices } = await this.paymentService.getPrices();
+  //   return {
+  //     stripePrices,
+  //   };
+  // }
 
-  @Post('createProduct')
-  async createProduct(@Body() productData: ProductData): Promise<any> {
-    const product = await this.paymentService.createProduct(productData);
-    return {
-      product,
-    };
-  }
+  // @Post('createProduct')
+  // async createProduct(@Body() productData: ProductData): Promise<any> {
+  //   const product = await this.paymentService.createProduct(productData);
+  //   return {
+  //     product,
+  //   };
+  // }
 
-  @Get('products')
-  async getProducts(): Promise<any> {
-    const products = await this.paymentService.getProducts();
-    return {
-      products,
-    };
-  }
+  // @Get('products')
+  // async getProducts(): Promise<any> {
+  //   const products = await this.paymentService.getProducts();
+  //   return {
+  //     products,
+  //   };
+  // }
 
-  @Post('createCheckoutSession')
-  async createCheckoutSession(@Body() productData: { name: string, price: number, currency: string }) {
-    const sessionId = await this.paymentService.createCheckoutSession(productData);
-    return { sessionId };
-  }
+  // @Post('createCheckoutSession')
+  // async createCheckoutSession(@Body() productData: { name: string, price: number, currency: string }) {
+  //   const sessionId = await this.paymentService.createCheckoutSession(productData);
+  //   return { sessionId };
+  // }
 }
