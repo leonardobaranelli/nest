1fadd9cd04766c45cb637d546c0d4a3718c0d112
Santiago Cortina <santicortina@hotmail.com>
2023-11-04 16:33:35 -0300

    card

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import {useState} from "react"
+/* import {useState} from "react"
 import Search from "../Search/SearchBar";
 import Link from "next/link";
 import { Post } from "@/redux/features/PostSlice";
@@ -74,6 +74,51 @@ const Navbar: React.FC<NavbarProps> = ({ busqueda }) => {
             </div>
             </div>
         </nav>
+    </div>
+  );
+};
+
+export default Navbar; */
+
+
+import React from "react";
+import Link from "next/link";
+import Search from "../Search/SearchBar";
+
+const Navbar = () => {
+  return (
+    <div className="p-4 bg-[#fc9a84]">
+      <nav className="flex items-center justify-between sm:h-10">
+        <div>
+        <Search/>
+        </div>
+        <div className="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
+          <Link
+            href="../../Views/Buy"
+            className="font-medium text-gray-500 hover:text-gray-900"
+          >
+            Venta
+          </Link>
+          <Link
+            href="../../Views/Rent"
+            className="font-medium text-gray-500 hover:text-gray-900"
+          >
+            Alquiler
+          </Link>
+          <Link
+            href="../../Views/FormCreate"
+            className="font-medium text-gray-500 hover:text-gray-900"
+          >
+            Publicar Inmueble
+          </Link>
+          <Link
+            href=""
+            className=" font-medium text-indigo-600 hover:text-indigo-500"
+          >
+            Log in
+          </Link>
+        </div>
+      </nav>
     </div>
   );
 };
