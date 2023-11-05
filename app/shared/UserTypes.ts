d81f9f90df69c153dfb780a5923964f5cbb118c2
leonardobaranelli <leobaranelli@gmail.com>
2023-11-05 15:28:26 -0300

    feat: userTypes

---
+++ app/shared/UserTypes.ts
@@ -0,0 +1,28 @@
+export interface User {
+    username: string;
+    email: string;
+    firstName: string;
+    lastName: string;
+    phone?: number;
+    personalId?: number;
+}  
+
+export interface Register {
+    username: string;
+    email: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    phone?: number;
+    personalId?: number;
+}
+
+export interface Login {
+    email: string;
+    password: string;
+}
+
+export interface UserState {
+    isAuthenticated: boolean;
+    user: User | null;
+  }
