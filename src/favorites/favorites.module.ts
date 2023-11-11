539e5052c18811c094671e4eac55fe00e519b9ac
JuanMPaola <juanmpaola25@gmail.com>
2023-11-11 19:32:11 -0300

    works

--- src/favorites/favorites.module.ts
+++ src/favorites/favorites.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { FavoritesService } from './favorites.service';
 import { FavoritesController } from './favorites.controller';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Favorite } from 'src/shared/models';
 
 @Module({
+  imports: [SequelizeModule.forFeature([Favorite])],
   controllers: [FavoritesController],
   providers: [FavoritesService],
 })
