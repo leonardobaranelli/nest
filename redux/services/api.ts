c3d2ab80822857056fbcd028f9137259983a8bc2
TUrielRios <riostiziano6@gmail.com>
2023-11-12 01:32:24 -0300

    score

--- redux/services/api.ts
+++ redux/services/api.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 require('dotenv').config();
 
-const { DEPLOY_BACK_URL } = process.env;
+const  DEPLOY_BACK_URL = 'http://localhost:3001';
 
 export interface Post {
   days: number | null;
@@ -21,12 +21,22 @@ export interface Post {
   id: string;
   images: string[];
   userId: string | null;
+  score: number | null;
+}
+export interface Score {
+  id: string,
+  type: string
+  score: string;
+  feedBack: number;
+  postId: string;
+  userId: string;
 }
 
 export const postsApi = createApi({
   reducerPath: "postsApi",
   refetchOnFocus: true,
-  baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_BACKEND_URL}),  
+  baseQuery: fetchBaseQuery({ baseUrl: DEPLOY_BACK_URL }),
+  //baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3001" }),
   
   endpoints: (builder) => ({
     getPostsByCondition: builder.query<Post[], string>({
@@ -62,6 +72,19 @@ export const postsApi = createApi({
         method: "DELETE",
       }),
     }),
+    getScore: builder.query<number | null, string>({
+      query: (postId) => `score/${postId}`, // Ruta actualizada para obtener el score de un post
+    }),
+    getReviews: builder.query<Score[], string>({
+      query: (postId) => `score/${postId}`, // Ruta actualizada para obtener las reseñas de un post
+    }),
+    createScore: builder.mutation<Score, Partial<Score>>({
+      query: (newScore) => ({
+        url: 'score/create', // Ruta actualizada para crear un score
+        method: 'POST',
+        body: newScore,
+      }),
+    }),
   }),
 });
 
@@ -72,4 +95,7 @@ export const {
   useGetPostQuery, // GET one
   useUpdatePostMutation, // PATCH (Update)
   useDeletePostMutation, // DELETE
+  useGetScoreQuery, // GET score of a post
+  useGetReviewsQuery, // GET reviews for a specific post
+  useCreateScoreMutation, // POST a review to a post
 } = postsApi;
